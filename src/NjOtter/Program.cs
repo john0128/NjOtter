ca5723b5ce5008c3fbc9a2bf09209bff97ef57d9
Nine <79080468+john0128@users.noreply.github.com>
2022-01-03 13:29:17 +0900

    Add files via upload

---
+++ src/NjOtter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace NjOtter
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Console.Title = "NjOtter CLI Enviornment";
+            string doc = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string folpath = doc + "/NjOtter Saves";
+            DirectoryInfo di = new DirectoryInfo(folpath);
+            if (di.Exists == false)
+            {
+                di.Create();
+            }
+            Main main = new Main();
+            main.Start();
+        }
+    }
+}
